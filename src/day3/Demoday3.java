dd8c22b7e602d6b10304c7ffddceb768fc61b885
CynthiaGCamacho <140673260+CynthiaGCamacho@users.noreply.github.com>
2023-07-29 11:09:25 -0400

    Update Demoday3.java

--- src/day3/Demoday3.java
+++ src/day3/Demoday3.java
@@ -3,7 +3,8 @@ package day3;
 public class Demoday3 {
 	public static void main(String []args) {
 		System.out.println("Cynthia Camacho");
-		
+		String country = "USA";
+		String state = "VA";
 	}
 
 }
