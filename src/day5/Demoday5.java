c4539c76c2bf192fc8f19422b0e9eb85608d534c
cynthia <cynthia@LAPTOP-G4CRUIVD.fios-router.home>
2023-07-29 13:05:14 -0400

    created a demoday5 class

---
+++ src/day5/Demoday5.java
@@ -0,0 +1,13 @@
+package day5;
+
+public class Demoday5 {
+	public static void main(String[]ars) {
+		System.out.println("Today is Sunny....");
+		
+		public void getInfo() {
+			
+			System.out.println("Today is Javaday....");
+		}
+	}
+
+}
